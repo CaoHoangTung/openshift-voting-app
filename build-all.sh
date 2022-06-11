7785d879eabb7ce90aa61c80de95b2f1d8faa602
Tung <tungch@b0be834eb873.ant.amazon.com>
2022-06-11 21:54:38 +0700

    update build-all.sh

--- build-all.sh
+++ build-all.sh
@@ -1,14 +1,14 @@
 IMAGE_TAG=latest
 
-docker build -t $IMAGE_REPO_NAME_VOTE .
-docker build -t $IMAGE_REPO_NAME_RESULT .
-docker build -t $IMAGE_REPO_NAME_WORKER .
+docker build -t $IMAGE_REPO_NAME_VOTE ./voting-app/vote
+docker build -t $IMAGE_REPO_NAME_RESULT ./voting-app/result
+docker build -t $IMAGE_REPO_NAME_WORKER ./voting-app/worker
 
 echo "Build done"
 
-docker tag $IMAGE_REPO_NAME_VOTE $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME_VOTE:$IMAGE_TAG
-docker tag $IMAGE_REPO_NAME_RESULT $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME_RESULT:$IMAGE_TAG
-docker tag $IMAGE_REPO_NAME_WORKER $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME_WORKER:$IMAGE_TAG
+docker tag $IMAGE_REPO_NAME_VOTE:$IMAGE_TAG $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME_VOTE:$IMAGE_TAG
+docker tag $IMAGE_REPO_NAME_RESULT:$IMAGE_TAG $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME_RESULT:$IMAGE_TAG
+docker tag $IMAGE_REPO_NAME_WORKER:$IMAGE_TAG $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME_WORKER:$IMAGE_TAG
 
 echo "Tag done"
 
